7d62de30a5bc5a9bd3e1564d0f71491d8705cea4
Choudhary Abdullah <magnetonx@gmail.com>
2025-07-11 02:42:39 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## SuperDev Docs
+
+SuperDev Pro is a 14-in-1 browser extension that helps developers and designers inspect and edit any website 3x faster, used by 6000+ professionals around the world to ship world-class websites.
---
+++ theme.config.tsx
@@ -0,0 +1,132 @@
+import Image from 'next/image';
+import {useConfig} from 'nextra-theme-docs';
+import {useRouter} from 'next/router';
+
+let site = {
+	base: 'https://docs.superdevpro.com',
+};
+
+export default {
+	head() {
+		let {asPath} = useRouter();
+		let {title} = useConfig();
+		let isHome = asPath === '/';
+		let meta = {
+			url: `https://docs.superdevpro.com${asPath}`,
+			title: isHome ? 'SuperDev Docs' : `${title} | SuperDev Docs`,
+			description: 'Find SuperDev Pro installation, activation, and device management documentation on this website.',
+			image: 'https://docs.superdevpro.com/meta/og.png',
+		};
+
+		return (
+			<>
+				<title>{meta.title}</title>
+				<meta
+					name='description'
+					content={meta.description}
+				/>
+				<link
+					rel='canonical'
+					href={meta.url}
+				/>
+				<meta
+					property='og:title'
+					content={meta.title}
+				/>
+				<meta
+					property='og:description'
+					content={meta.description}
+				/>
+				<meta
+					property='og:url'
+					content={meta.url}
+				/>
+				<meta
+					property='og:site_name'
+					content='SuperDev Pro'
+				/>
+				<meta
+					property='og:image'
+					content={meta.image}
+				/>
+				<meta
+					property='og:type'
+					content='website'
+				/>
+				<meta
+					name='twitter:card'
+					content='summary_large_image'
+				/>
+				<meta
+					name='twitter:site'
+					content='@superdevpro'
+				/>
+				<meta
+					name='twitter:title'
+					content={meta.title}
+				/>
+				<meta
+					name='twitter:description'
+					content={meta.description}
+				/>
+				<meta
+					name='twitter:image'
+					content={meta.image}
+				/>
+				<link
+					rel='shortcut icon'
+					href='/meta/favicon.png'
+				/>
+				<link
+					rel='icon'
+					href='/meta/favicon.png'
+				/>
+				<link
+					rel='apple-touch-icon'
+					href='/meta/favicon.png'
+				/>
+				<meta
+					name='theme-color'
+					content='#09090b'
+				/>
+			</>
+		);
+	},
+	nextThemes: {
+		defaultTheme: 'dark',
+	},
+	backgroundColor: {
+		dark: '009,009,011',
+		light: '255,255,255',
+	},
+	logo: (
+		<>
+			<Image
+				alt='SuperDev Pro Logo'
+				src='/logo/logo.png'
+				width='30'
+				height='30'
+			/>
+			<div>SuperDev Docs</div>
+		</>
+	),
+	logoLink: site.base,
+	sidebar: {
+		defaultMenuCollapseLevel: 1,
+		autoCollapse: true,
+		toggleButton: false,
+	},
+	editLink: {
+		component: null,
+	},
+	feedback: {
+		content: null,
+	},
+	footer: {
+		component: null,
+	},
+	gitTimestamp: null,
+	themeSwitch: {
+		component: null,
+	},
+};
